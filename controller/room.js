1dc610b13dbea15bdab04cbc3ad2f29d2c3b456a
LAPTOP-I427NBL1\Suhan <614234024@parichat.skru.ac.th>
2021-10-21 16:32:19 +0700

    เพิ่ม function room

--- controller/room.js
+++ controller/room.js
@@ -5,30 +5,52 @@ const prisma = new PrismaClient()
 async function getRooms(req, res) {
   const rooms = await prisma.room.findMany()
 
-  res.send(rooms)
+  res.json(rooms)
 }
 
 async function addRoom(req, res) {
-  const number = req.body.number
-  const bedSize = req.body.bedSize
-  const price = req.body.price
-  const roomTypeID = req.body.roomTypeID
+  const number = req.body.room_number
+  const price = req.body.room_price
+  const typeID = req.body.room_type_id
 
   await prisma.room.create({
     data: {
       room_number: number,
-      room_bed_size: bedSize,
       room_price: price,
-      room_type_id: roomTypeID,
+      room_type_id: typeID,
     },
   })
 
-  res.send('เสร็จสิ้น')
+  res.status(201).end()
 }
 
-async function updateRoom(req, res) {}
+async function updateRoom(req, res) {
+  const roomID = req.body.room_id
+  const price = req.body.room_price
 
-async function deleteRoom(req, res) {}
+  await prisma.room.update({
+    where: {
+      room_id: roomID,
+    },
+    data: {
+      room_price: price,
+    },
+  })
+
+  res.status(200).end()
+}
+
+async function deleteRoom(req, res) {
+  const roomID = req.body.room_id
+
+  await prisma.room.delete({
+    where: {
+      room_id: roomID,
+    },
+  })
+
+  res.status(204).end()
+}
 
 module.exports = {
   getRooms,
